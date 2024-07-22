000e34ed4ab7dea60a31587777eb3c7f6f3761e7
lukasz-mac <luka.koscik@gmail.com>
2024-07-22 22:13:39 +0200

    change methods to work on single key, code cleanuo

--- linked-list.ts
+++ linked-list.ts
@@ -1,6 +1,6 @@
 import {KeyValuePair} from "./index";
 
-type NodeType = {
+export type NodeType = {
     value: KeyValuePair | null,
     next: NodeType | null,
 }
@@ -9,137 +9,137 @@ const Node = (value?: KeyValuePair): NodeType => {
     return ({
         value: value ?? null,
         next: null,
-    })
+    });
 
-}
+};
 
-const LinkedList = (value?: KeyValuePair) => {
+export const convertKeyValuePairToString = (value: KeyValuePair | null) => {
+    return value ? value.join(' ') : null;
+};
+
+export const LinkedList = (value?: KeyValuePair) => {
     let list = value ? Node(value) : null;
 
-    const getLinkedList = () => list
+    const getLinkedList = () => list;
 
     const traverseToLastNode = (node = list): NodeType | null => {
         if (!node) {
-            return null
+            return null;
         }
         if (!node.next) {
-            return node
+            return node;
         }
-        return traverseToLastNode(node.next)
-    }
+        return traverseToLastNode(node.next);
+    };
 
     const append = (value: KeyValuePair) => {
         if (!list) {
-            list = Node(value)
-            return
+            list = Node(value);
+            return;
         }
-        traverseToLastNode()!.next = Node(value)
-    }
+        traverseToLastNode()!.next = Node(value);
+    };
 
 
     const prepend = (value: KeyValuePair) => {
         if (!list) {
-            list = Node(value)
-            return
+            list = Node(value);
+            return;
         }
 
-        const newList = Node(value)
-        newList.next = list
-        list = newList
+        const newList = Node(value);
+        newList.next = list;
+        list = newList;
 
-    }
+    };
 
-    const head = () => list
+    const head = () => list;
 
     const size = () => {
         if (!list) {
-            return 0
+            return 0;
         }
-        let nodeCount = 1
-        let currentNode = list
+        let nodeCount = 1;
+        let currentNode = list;
         while (currentNode.next) {
-            nodeCount += 1
-            currentNode = currentNode.next
+            nodeCount += 1;
+            currentNode = currentNode.next;
         }
         return nodeCount;
-    }
+    };
 
     const tail = () => {
-        return traverseToLastNode()
-    }
+        return traverseToLastNode();
+    };
 
     const at = (index: number) => {
         if (!index) {
-            return list
+            return list;
         }
         let currentIndex = 0;
-        let currentNode = list
+        let currentNode = list;
         while (currentIndex < index && !!currentNode?.next) {
             currentIndex++;
-            currentNode = currentNode!.next
+            currentNode = currentNode!.next;
         }
 
         if (currentIndex === index) {
-            return currentNode
+            return currentNode;
         }
-        return null
-    }
+        return null;
+    };
 
     const pop = () => {
         if (!list) {
-            return
+            return;
         }
         if (!list?.next) {
-            list = null
-            return
+            list = null;
+            return;
         }
-        let secondToLastNode = list
-        let lastNode: NodeType | null = list.next
+        let secondToLastNode = list;
+        let lastNode: NodeType | null = list.next;
         while (!!lastNode.next) {
-            secondToLastNode = lastNode
-            lastNode = lastNode.next
+            secondToLastNode = lastNode;
+            lastNode = lastNode.next;
         }
 
-        secondToLastNode.next = null
-    }
+        secondToLastNode.next = null;
+    };
 
-    const convertKeyValuePairToString = (value: KeyValuePair | null) => {
-        return value ? value.join(' ') : null
-    }
 
-    const contains = (value: KeyValuePair, shouldReturnNode = false) => {
+    const contains = (key: string, shouldReturnNode = false) => {
         let currentNode = list;
         if (!currentNode) {
-            return false
+            return false;
         }
-        const searchedValue = convertKeyValuePairToString(value)
+
 
         while (currentNode) {
-            const currentValue = convertKeyValuePairToString(currentNode.value)
-            if (!!currentValue && currentValue === searchedValue) {
-                return shouldReturnNode ? currentNode : true
+            const [currentValue] = currentNode.value ?? [''];
+            if (!!currentValue && currentValue === key) {
+                return shouldReturnNode ? currentNode : true;
             }
-            currentNode = currentNode.next
+            currentNode = currentNode.next;
         }
 
-        return false
-    }
+        return false;
+    };
 
-    const find = (value: KeyValuePair) => {
-        const searchedValue = convertKeyValuePairToString(value)
+    const find = (key: string) => {
         let currentIndex = 0;
         let currentNode = list;
         while (currentNode) {
-            const currentValue = convertKeyValuePairToString(currentNode.value)
-            if (!!currentValue && currentValue === searchedValue) {
-                return currentIndex
+            const [currentValue] = currentNode?.value ?? [''];
+            if (!!currentValue && currentValue === key) {
+                return currentIndex;
             }
             currentIndex += 1;
-            currentNode = currentNode.next
+            currentNode = currentNode.next;
 
         }
         return null;
-    }
+    };
 
     return {
         append,
@@ -151,9 +151,10 @@ const LinkedList = (value?: KeyValuePair) => {
         head,
         at,
         pop,
-        convertKeyValuePairToString,
         contains,
         find
-    }
-}
+    };
+};
+
+export type LinkedListType = ReturnType<typeof LinkedList>
 
